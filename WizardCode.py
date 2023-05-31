afe39f24a71e463484ac4a0e03e923f10be0ce32
bleer <125562229+bleer2004@users.noreply.github.com>
2023-05-31 11:59:56 -0300

    mudeu a sia mae

--- WizardCode.py
+++ WizardCode.py
@@ -3,6 +3,8 @@ import random
 import time
 import mysql.connector
 
+buce3ta
+
 # conecta ao banco de dados ""aqui tem q os dados do banco de dados da maquina de vcs ou a q vai ser host do jogo""
 dbperguntas = mysql.connector.connect(
     host="localhost",
